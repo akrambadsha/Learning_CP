53a8df34f459f502f5f759d2c821ba8bdd66a283
akrambadsha <amirbadsha014@gmail.com>
2025-06-25 00:36:19 +0600

    E. Area of a Circle

---
+++ codeforces/ICPC Assiut University Community/Sheet #1 (Data type - Conditions)/E_Area_of_a_Circle.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double r, area;
+    const double pi= 3.141592653;
+    cin >> r;
+    area = pi * r * r;
+    cout << fixed << setprecision(9) << area;
+    return 0;
+ 
+}
