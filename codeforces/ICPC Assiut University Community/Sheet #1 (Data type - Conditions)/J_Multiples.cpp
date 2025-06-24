167925a7d2408f29d16de7beed6cc379aa38088e
akrambadsha <amirbadsha014@gmail.com>
2025-06-24 23:14:31 +0600

    J_Multiples

---
+++ codeforces/ICPC Assiut University Community/Sheet #1 (Data type - Conditions)/J_Multiples.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int A, B ;
+    cin >> A >> B ;
+    if ((A%B == 0)||(B%A == 0))
+    cout << "Multiples";
+    else
+    cout << "No Multiples";
+    return 0;
+
+}
