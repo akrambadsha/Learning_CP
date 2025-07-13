da8161e4efb73c737be69233d8f9435ba382a85f
akrambadsha <amirbadsha014@gmail.com>
2025-07-13 20:10:34 +0600

    N. Numbers Histogram

---
+++ codeforces/ICPC Assiut University Community/Sheet #2 (Loops)/N_Numbers_Histogram.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char s;
+    cin >> s;
+    int n;
+    cin >> n;
+    while(n--)
+    {
+        int n;
+        cin >> n;
+        for(int i=1; i<=n; i++)
+        {
+            cout << s;
+        }
+        cout << endl;
+    }
+    return 0;
+}
